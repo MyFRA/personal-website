f66fe73b79811f292555eb6233784d4ec39580b3
To My <myfragithub@gmail.com>
2021-04-18 10:09:39 +0700

    initial commit

---
+++ public/js/script.js
@@ -0,0 +1,7 @@
+function setLogoColor() {
+    
+    if( document.querySelector('.inner-nav .logo-wrapper h1') != null ) {
+        const logo = document.querySelector('.inner-nav .logo-wrapper h1');
+        console.log(logo.innerText);
+    }
+}
