678c6601607c00c60ce14a36e0c7490d06184a74
MyFRA <myfragithub@gmail.com>
2021-06-12 13:31:57 +0700

    update

--- public/js/script.js
+++ public/js/script.js
@@ -16,21 +16,9 @@ const listAchievments = document.querySelector('ul.list-achievments');
 listAchievments.addEventListener('click', (e) => {
     if( e.target.classList.contains('achievment') ) {
         const achievmentDetail = Array.from(e.target.children).find(e => e.classList.contains('achievment-detail'));
-        const listAchievmentsDetail = listAchievments.querySelectorAll('li.achievment .achievment-detail');
-        listAchievmentsDetail.forEach((e) => {
-            if( e != achievmentDetail && !e.classList.contains('d-none') ) {
-                e.classList.add('d-none');
-            } 
-        });
         (achievmentDetail != null) ? achievmentDetail.classList.toggle('d-none') : '';
     } else if(e.target.tagName == 'SPAN') {
         const achievmentDetail = Array.from(e.target.parentElement.children).find(e => e.classList.contains('achievment-detail'));
-        const listAchievmentsDetail = listAchievments.querySelectorAll('li.achievment .achievment-detail');
-        listAchievmentsDetail.forEach((e) => {
-            if( e != achievmentDetail && !e.classList.contains('d-none') ) {
-                e.classList.add('d-none');
-            } 
-        });
         (achievmentDetail != null) ? achievmentDetail.classList.toggle('d-none') : '';
     }
 });
